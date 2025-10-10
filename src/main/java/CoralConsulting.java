5bc4de8f065fad3ccde00132315ffee6084dd020
Coral Estrada <coralestrada28@gmail.com>
2025-10-10 15:58:53 -0500

    Added main display scanner.

--- src/main/java/CoralConsulting.java
+++ src/main/java/CoralConsulting.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class CoralConsulting {
     public static void main(String[] args) {
+        // Declare Variables
+        Scanner scanner = new Scanner(System.in);
+        // This boolean is for the while do-while loop below
+        boolean repeatLoop = true;
+        char homePageInput = scanner.nextLine().trim().toLowerCase().charAt(0);
+        System.out.println("""
+                    Hello!
+                    Please select an option from the following: 
+                    (D) Add a Deposit
+                    (P) Make a Payment
+                    (L) See Ledger
+                    (X) Close Application
+                    """);
 
     }
 }
